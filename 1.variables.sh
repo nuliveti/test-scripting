f7e6c337879fca7fd1c18c444d9fa545d18205b1
kiran <rayoflight890@gmail.com>
2023-09-01 22:55:46 +0530

    created another file

--- 1.variables.sh
+++ 1.variables.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-excho "
+
+echo "iam learning devops and aws"
+
+##EX: below case shell script run the command inside $() and store the output into variables called DATE
+DATE=$(date)
+
+echo "hellow, this script excuted at timestamp: =$DATE"
