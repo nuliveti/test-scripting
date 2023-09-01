daf68d57855ded8a97b734b7362ae82e7c7c6d2a
kiran <rayoflight890@gmail.com>
2023-09-01 23:12:43 +0530

    created another file

---
+++ 3.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###ex:2 we can always get the values of variables from command line
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hi $PERSON2, good morning"
+echo "$PERSON2: goor morning $PERSON1"
+echo "$PERSON1: hoe ar u $PERSON2"
+echo "$PERSON2: iam going good, how ar u $PERSON1"
