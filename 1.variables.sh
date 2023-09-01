189a3a654fd388465a8e34a65692d12492f55851
kiran <rayoflight890@gmail.com>
2023-09-01 23:08:45 +0530

    created another file

--- 1.variables.sh
+++ 1.variables.sh
@@ -6,4 +6,4 @@ echo "iam learning devops and aws"
 ##EX: below case shell script run the command inside $() and store the output into variables called DATE
 DATE=$(date)
 
-echo "hellow, this script excuted at timestamp: =$DATE"
+echo "hellow, this script excuted at timestamp: $DATE"
---
+++ 2.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "hellow world, iam learning devops with aws"
+
+PERSON1=KIRAN
+PERSON2=KEERTHI
+
+echo "$PERSON1: hi $PERSON2, good morning"
+echo "$PERSON2: goor morning $PERSON1"
+echo "$PERSON1: hoe ar u $PERSON2"
+echo "$PERSON2: iam going good, how ar u $PERSON1"
