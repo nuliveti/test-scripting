a246b857eb91b33f4692b59d9e0673ed2bb29bcd
kiran <rayoflight890@gmail.com>
2023-09-02 08:00:09 +0530

    addition example

---
+++ 6.array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#### this is called array ...instead of single value it can hold multiple values
+FRUITS=("APPLE" "orange" "banana")
+###inside the array we always refer values with index and it starts from 0
+
+echo "first fruit :${FRUITS[0]}"
+echo "all fruits: ${fruits[@]}"
