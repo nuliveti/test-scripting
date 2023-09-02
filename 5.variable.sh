16fa772c37ee05166d7c67dc022fb32af42b7f14
kiran <rayoflight890@gmail.com>
2023-09-02 07:34:19 +0530

    addition example

---
+++ 5.variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your username"
+read -s USERNAME # this is the variable where the value of the one user entered
+
+echo "username entered is : $USERNAME"
+
+echo "please enter your password"
+read -s PASSWORD # this is the variable where the value of the one user entered
+
+echo "username entered is : $PASSWORD"
