bafd867aca51f53271b830e14bbfad7eda30a65c
kiran <rayoflight890@gmail.com>
2023-09-02 13:08:08 +0530

    addition example

---
+++ 8.git instalation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# our goal is to install git 
+# first check root user or not 
+#if not root user exit programme & inform user to run with sudo accesss
+#if root user then install mysql.
+
+USERID=$(id -u)
+if [ $USERID -ne 0]
+
+then
+    echo "ERROR: please run this script as root user"
+fi
+
+yum install git -y
