5207c4f2e9a1763332194de0ae9ac30aa4573b12
kiran <rayoflight890@gmail.com>
2023-09-03 15:33:52 +0530

    addition example

--- 11.loops.sh
+++ 11.loops.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 
-echo 1
-echo 2
-echo 3
-echo 4
+# echo 1
+# echo 2
+# echo 3
+# echo 4
+
+
+##below is the example for 100 and we can change it any time as per requirement
+
+for i in {1..100}
+do
+  echo $i
+done  
