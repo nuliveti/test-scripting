5f85c942cdfa8a4cb6943f7f2aab72a1ca6f5337
kiran <rayoflight890@gmail.com>
2023-09-04 10:03:40 +0530

    test script

--- 12.testscript.sh
+++ 12.testscript.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+
 #### im declaring all useful variavbles here
-USERID=$(id -u)
 DATE=$(date +%F)
 LOGDIR=/home/centos/test-scripting/script_logs
-LOGFILE=$LOGDIR/$0-$DATE.log
 SCRIPT_NAME=$0
+LOGFILE=$LOGDIR/$0-$DATE.log
+USERID=$(id -u)
+
+
 B="\e[36m"
 P="\e[35m"
 N="\e[0m"
@@ -24,12 +27,13 @@ fi
 VALIDATE(){
     if [ $1 -ne 0];
     then
-    echo -e "$P Installing $2.....$P failure $N"
+    echo -e "Installing $2.....$P failure $N"
     exit 1
     else
-    echo -e "$P Installing $2......$Y SUCCESS $N"
+    echo -e "Installing $2......$Y SUCCESS $N"
     fi
 }
+
 for i in $@
 do 
    yum list installed $i &>>$LOGFILE
