3dd4e8ca6c0a58f8f4077c0bf6b15e92d5c38d0d
kiran <rayoflight890@gmail.com>
2023-09-04 09:42:41 +0530

    test script

--- 11.loops.sh
+++ 11.loops.sh
@@ -9,7 +9,22 @@
 
 ##below is the example for 100 and we can change it any time as per requirement
 
-for i in {1..100}
-do
-  echo $i
-done  
+# for i in {1..100}
+# do
+#   echo $i
+# done  
+
+#all argumants are in $@
+
+for i in $@
+do 
+   yum install $i -y
+   done
+
+# A. improvements
+# b. implement log files
+# implement colours
+# implement validaTIONS
+# implement validations through functions
+# your script should check package already installed or not, if already installed print yellow colour
+# if installed just print package already installed, it should not run install command.
---
+++ 12.testscript.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#### im declaring all useful variavbles here
+USERID=$(id -u)
+DATE=&(date +%F)
+LOGDIR=/home/centos/test-scripting/script_logs
+LOGFILE=$LOGDIR/$0-$DATE.log
+SCRIPT_NAME=$0
+B="\e[36m"
+P="\e[35m"
+N="\e[0m"
+Y="\e[33m"
+
+
+
+if [ $USERID -ne 0 ]{
+
+then
+
+    echo -e "$B user dont have root privilege $N"
+    exit 1
+
+}
+fi
+### here im loopig all packages togeather
+VALIDATE(){
+    if [ $1 -ne 0];
+    then
+    echo -e "$P Installing $2.....$P failure $N"
+    exit 1
+    else
+    echo -e "Installing $2......$Y SUCCESS $N"
+    fi
+}
+for i in $@
+do 
+   yum list installed $i &>>$LOGFILE
+   if [ $? -ne 0]
+   then
+       echo -e " $P $i is not installed, let's install it $N"
+   yum install $i -y &>>$LOGFILE
+   VALIDATE $? "$i"
+   else
+       echo -e "$Y $i is already installed $N"
+
+fi
+done
+
+
+
