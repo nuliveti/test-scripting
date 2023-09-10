dfa37339ef9b9d8bd506ac5f08243c12bba36cc7
neeliveti <neeliveti.kirankumar@gmail.com>
2023-09-10 13:49:59 +0530

    server creation

--- create-servers.sh
+++ create-servers.sh
@@ -18,8 +18,7 @@ do
        INSTANCE_TYPE="t2.micro"
     fi 
    echo "creating $i instance"
-   IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SUCURYTY_GROUP_ID
-   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].
+   IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SUCURYTY_GROUP_ID--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].
    privateIpAddress')
    echo "created $i instancec: $IP_ADDRESS"
    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
@@ -28,7 +27,7 @@ do
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
-                                    "Name": "$i.$'$i.$DOMAIN_NAME'",
+                                    "Name": "'$i.$'$i.$DOMAIN_NAME'",
                                     "Type": "A",
                                     "TTL": 300,
                                  "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
